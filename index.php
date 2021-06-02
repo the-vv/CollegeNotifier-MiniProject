6e98e391957dd7f58960b3d4f4573e6d1b2330be
vv-2001 <vishnuvinod2772001@gmail.com>
2021-06-03 00:23:42 +0530

    added routing

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
-require('./common/header.html');
+require __DIR__ . './common/header.html' ;
 
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/admin' :
+        require __DIR__ . '/views/adminIndex.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
 
-
-require('./common/footer.html');
+require __DIR__ . './common/footer.html' ;
---
+++ views/404.php
@@ -0,0 +1,1 @@
+The page looking for is not found
