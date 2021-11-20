cf2f199b948a800420ccd7e16030eeb7c90ddeea
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2021-11-21 01:33:42 +0530

    all error fixed

--- views/students/events_view.php
+++ views/students/events_view.php
@@ -41,7 +41,7 @@ function eventItem($e)
     global $student;
     ?>
 
-<li class='list-group-item d-flex justify-content-between align-items-center'>
+<li class='list-group-item d-flex justify-content-between align-items-center eid-<?php echo $e['id'] ?>'>
     <a onclick="showEvent('<?php echo $e['id'] ?>')" class="d-inline-block text-truncate h6 text-decoration-none">
         <div class="row">
             <div class="col-auto d-flex align-items-center">
